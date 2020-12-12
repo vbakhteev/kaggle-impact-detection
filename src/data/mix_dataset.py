6f172b1bfd4915a2100764b9446deec36798de8e
Vladislav Bakhteev <bakhteev_v@mail.ru>
2020-12-12 15:14:52 +0300

    Mosaic for images

--- src/data/mix_dataset.py
+++ src/data/mix_dataset.py
@@ -8,7 +8,7 @@ from src.data.transforms import mixup_video, cutmix_video, cutmix_image
 class MixDataset(Dataset):
     def __init__(self, dataset, video=True):
         self.dataset = dataset
-        self.cutmix_fn = cutmix_video if video else cutmix_image
+        self.video = video
 
     def __getitem__(self, index):
         images1, target1 = self.dataset[index]
@@ -18,14 +18,30 @@ class MixDataset(Dataset):
         images2, target2 = self.dataset[random_index]
         boxes2, labels2 = target2['boxes'], target2['labels']
 
+        # MixUp
         if random.random() < 0.5:
             images, boxes, labels = mixup_video(
                 images1, images2, boxes1, boxes2, labels1, labels2
             )
+
+        # CutMix
         else:
-            images, boxes, labels = self.cutmix_fn(
-                images1, images2, boxes1, boxes2, labels1, labels2
-            )
+            samples = [
+                (images1, boxes1, labels1),
+                (images2, boxes2, labels2),
+            ]
+
+            if self.video:
+                images, boxes, labels = cutmix_video(samples)
+
+            # If image then construct mosaic of 4 images
+            else:
+                for _ in range(2):
+                    random_index = random.randint(0, len(self.dataset) - 1)
+                    images_i, target_i = self.dataset[random_index]
+                    boxes_i, labels_i = target_i['boxes'], target_i['labels']
+                    samples += [(images_i, boxes_i, labels_i)]
+                images, boxes, labels = cutmix_image(samples)
 
         return images, {'boxes': boxes, 'labels': labels}
 
--- src/data/transforms.py
+++ src/data/transforms.py
@@ -13,13 +13,74 @@ def mixup_video(images1, images2, boxes1, boxes2, labels1, labels2):
 
 
 @torch.no_grad()
-def cutmix_video(images1, images2, boxes1, boxes2, labels1, labels2):
+def cutmix_video(samples: list):
+    """
+    samples: [(images, boxes, labels)]. Len=2
+    """
     # TODO implement cutmix
+    images1, boxes1, labels1 = samples[0]
     return images1, boxes1, labels1
 
 
-def cutmix_image(image1, image2, boxes1, boxes2, labels1, labels2):
-    return image1, boxes1, labels1
+@torch.no_grad()
+def cutmix_image(samples):
+    """
+    samples: [(image, boxes, labels)]. Len=4
+    """
+    h, w = samples[0][0].shape[1:]
+    xs = w // 2
+    ys = h // 2
+
+    # center x, y
+    xc = int(random.uniform(w * 0.25, w * 0.75))
+    yc = int(random.uniform(h * 0.25, h * 0.75))
+
+    result_image = torch.full((3, h, w), 0, dtype=torch.float32)
+    result_boxes = []
+    result_labels = []
+
+    for i, sample in enumerate(samples):
+        image, boxes, labels = sample
+        boxes[:, [0, 1, 2, 3]] = boxes[:, [1, 0, 3, 2]]  # xyxy
+
+        if i == 0:
+            x1a, y1a, x2a, y2a = max(xc - w, 0), max(yc - h, 0), xc, yc  # xmin, ymin, xmax, ymax (large image)
+            x1b, y1b, x2b, y2b = w - (x2a - x1a), h - (y2a - y1a), w, h  # xmin, ymin, xmax, ymax (small image)
+        elif i == 1:  # top right
+            x1a, y1a, x2a, y2a = xc, max(yc - h, 0), min(xc + w, xs * 2), yc
+            x1b, y1b, x2b, y2b = 0, h - (y2a - y1a), min(w, x2a - x1a), h
+        elif i == 2:  # bottom left
+            x1a, y1a, x2a, y2a = max(xc - w, 0), yc, xc, min(ys * 2, yc + h)
+            x1b, y1b, x2b, y2b = w - (x2a - x1a), 0, max(xc, w), min(y2a - y1a, h)
+        elif i == 3:  # bottom right
+            x1a, y1a, x2a, y2a = xc, yc, min(xc + w, xs * 2), min(ys * 2, yc + h)
+            x1b, y1b, x2b, y2b = 0, 0, min(w, x2a - x1a), min(y2a - y1a, h)
+        result_image[:, y1a:y2a, x1a:x2a] = image[:, y1b:y2b, x1b:x2b]
+        padw = x1a - x1b
+        padh = y1a - y1b
+
+        boxes[:, 0] += padw
+        boxes[:, 1] += padh
+        boxes[:, 2] += padw
+        boxes[:, 3] += padh
+
+        result_boxes.append(boxes)
+        result_labels.append(labels)
+
+    result_boxes = torch.cat(result_boxes, 0)
+    result_labels = torch.cat(result_labels)
+    torch.clamp(result_boxes[:, 0], 0, 2 * xs, out=result_boxes[:, 0])
+    torch.clamp(result_boxes[:, 2], 0, 2 * xs, out=result_boxes[:, 2])
+    torch.clamp(result_boxes[:, 1], 0, 2 * ys, out=result_boxes[:, 1])
+    torch.clamp(result_boxes[:, 3], 0, 2 * ys, out=result_boxes[:, 3])
+    result_boxes = result_boxes.int()
+
+    boxes_to_use = (result_boxes[:, 2] - result_boxes[:, 0]) * (result_boxes[:, 3] - result_boxes[:, 1]) > 0
+    result_boxes = result_boxes[boxes_to_use]
+    result_labels = result_labels[boxes_to_use]
+
+    result_boxes[:, [0, 1, 2, 3]] = result_boxes[:, [1, 0, 3, 2]]  # yxyx
+    return result_image, result_boxes, result_labels
 
 
 class VideoTranform:
